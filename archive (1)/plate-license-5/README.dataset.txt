aa05381bbca22e25f7e86a3683cda7ab8651531e
rhit-nagalab <98065628+rhit-nagalab@users.noreply.github.com>
2025-08-01 10:26:46 -0400

    Add images and beginning of code

---
+++ archive (1)/plate-license-5/README.dataset.txt
@@ -0,0 +1,6 @@
+# plate license > 2023-12-12 1:31am
+https://universe.roboflow.com/mazen-alwaqdei/plate-license-dvt6r
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ main.m
@@ -0,0 +1,23 @@
+clear;
+
+trainDS = imageDatastore("archive (1)\plate-license-5\train\");
+testDS = imageDatastore("archive (1)\plate-license-5\test\");
+validDS = imageDatastore("archive (1)\plate-license-5\valid\");
+
+function readImages(dataStore)
+
+    nImages = numel(dataStore.Files);
+
+    for i = 1:1
+        [img, fileInfo] = readimage(dataStore, i);
+        grayImg = rgb2gray(img);
+        smoothImg= imgaussfilt(grayImg,3);
+        imtool(img);
+        imtool(smoothImg);
+    end
+
+end
+
+readImages(trainDS);
+
+
