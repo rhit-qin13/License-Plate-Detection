1e25221a41346bdcb7b45163ca3b55a79090c2ad
rhit-nagalab <98065628+rhit-nagalab@users.noreply.github.com>
2025-08-07 15:47:07 -0400

    Update ocr

--- ocrTest.py
+++ ocrTest.py
@@ -1,11 +1,12 @@
 from paddleocr import PaddleOCR
 
 
+
 ocr = PaddleOCR(use_angle_cls=True, lang='en') 
 
-imagePath = "archive (1)\plate-license-5\test\0_3_hr_png_jpg.rf.c679897f52689da139e29adc623291fd.jpg"
+imagePath = r"archive (1)\plate-license-5\test\0_3_hr_png_jpg.rf.c679897f52689da139e29adc623291fd.jpg"
 
-results = ocr.ocr(imagePath)
+results = ocr.predict(imagePath)
 
-for line in results[0]: 
-    print(f"Detected text: {line[1][0]}, Confidence: {line[1][1]}")
+for res in results:
+    print(res)
